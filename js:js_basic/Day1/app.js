04e32b4f3b7977ff1bb02c1ffd54afb624220e49
Evening <evening@admins-iMac.local>
2022-11-28 20:14:06 +0800

    School Nov-28

---
+++ js:js_basic/Day1/app.js
@@ -0,0 +1,368 @@
+// console.log("Hello World - 2");
+
+
+
+// No1
+
+// Comments can make code readable
+
+
+
+
+// No2
+
+// Javascript lesson begin
+
+
+
+
+// No3
+
+/*Comments 
+can 
+make 
+code 
+readable*/
+
+
+
+
+
+// No4
+
+let number = 123;
+
+let string = "String";
+
+let boolean = true;
+
+let nas;
+
+// let ner;
+
+
+
+
+// No5
+
+
+console.log(typeof (number));
+
+console.log(typeof (string));
+
+console.log(typeof (boolean));
+
+console.log(typeof (nas));
+
+
+
+
+
+// No6
+let nickName;
+
+console.log(nickName);
+
+
+
+
+// No7
+
+let nickname = "Eric";
+
+console.log(nickname);
+
+
+
+
+
+// No8
+
+let firstname = "Battamir";
+let lastname = "Enkhtur";
+let maritalstatus = "Single";
+let country = "Mongolia";
+let age = 21;
+
+console.log(firstname);
+console.log(lastname);
+console.log(maritalstatus);
+console.log(country);
+console.log(age);
+
+
+
+
+// No9
+
+console.log(firstname + " " + lastname + " " + maritalstatus + " " + country + " " + age);
+
+
+// No10
+
+let myage = 210;
+let yourage = 310;
+console.log(myage);
+console.log(yourage);
+
+// No11
+
+let x = 4;
+
+let y = 4;
+
+let z = 4;
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+console.log("p=", x + y + z);
+
+
+// No12
+
+let p = 4;
+
+let r = 2;
+
+console.log("s=", p * r ** 2);
+
+
+// No13
+
+
+let y1, y2, x2, x1;
+
+y1 = 10;
+y2 = 20;
+x1 = 50;
+x2 = 100;
+
+console.log("m=", y2 - y1 / x2 - x1);
+
+// No14
+
+let gb, b;
+
+gb = 15;
+
+b = 8589934592;
+
+bit = gb * b;
+
+console.log(bit);
+
+// No15
+
+
+
+// No16
+
+let side1, side2, side3;
+
+side1 = 3;
+side2 = 5;
+side3 = 10;
+
+console.log("S=", (side1 + side2 + side3) / 2);
+
+
+// No17
+
+let c;
+
+c = 10;
+
+console.log("f=", (c * 1.8) + 32);
+
+// No18
+
+let f;
+
+f = 53.6;
+
+console.log("c=", (f - 32) * 5 / 9);
+
+
+
+// No19
+
+let h1, h2;
+
+h1 = 5;
+h2 = 20;
+
+console.log(h1 + h2);
+console.log(h1 - h2);
+console.log(h1 * h2);
+console.log(h1 / h2);
+console.log(h1 % h2);
+
+
+
+// No20
+
+let cm;
+
+cm = 10;
+
+console.log("inch=", cm / 2.54);
+
+let inch;
+
+inch = 10;
+
+console.log("cm=", inch * 2.54);
+
+
+// No21
+
+let ra, d;
+
+ra = 2;
+
+d = ra * 2;
+
+const PTI = 3.14;
+
+console.log("v=", 4 / 3 * PTI * ra ** 3);
+
+console.log("c=", PTI * d);
+
+
+// No22
+
+let e, firstnumber, a;
+
+e = 123;
+firstnumber = e % 100;
+a = e - firstnumber;
+et=a/100;
+console.log("et=",et)
+
+
+// console.log("x=", a / 100);
+
+
+// No23
+
+
+let n1="Baby-Shark "; n2="doo-doo,doo-doo";
+n3="Mommy Shark"; n4="Daddy Shark";
+n5="Grandma Shark";n6="Grandpa Shark";
+n7="Let's go hunt";n8="Run away"; 
+n9="Safe at last"; n10="It's the end"; 
+
+
+console.log(n1+","+n2);
+console.log(n1+","+n2);
+console.log(n1+","+n2);
+console.log(n1);
+
+console.log(n3+","+n2);
+console.log(n3+","+n2);
+console.log(n3+","+n2);
+console.log(n3);
+
+console.log(n4+","+n2);
+console.log(n4+","+n2);
+console.log(n4+","+n2);
+console.log(n4);
+
+console.log(n5+","+n2);
+console.log(n5+","+n2);
+console.log(n5+","+n2);
+console.log(n5);
+
+console.log(n6+","+n2);
+console.log(n6+","+n2);
+console.log(n6+","+n2);
+console.log(n6);
+
+console.log(n7+","+n2);
+console.log(n7+","+n2);
+console.log(n7+","+n2);
+console.log(n7);
+
+console.log(n8+","+n2);
+console.log(n8+","+n2);
+console.log(n8+","+n2);
+console.log(n8);
+
+console.log(n9+","+n2);
+console.log(n9+","+n2);
+console.log(n9+","+n2);
+console.log(n9);
+
+console.log(n10+","+n2);
+console.log(n10+","+n2);
+console.log(n10+","+n2);
+console.log(n10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let name = "Battamir";
+
+// let age = 21;
+
+// let job;
+
+
+// console.log(name);
+// console.log(age);
+// console.log(job);
+
+// job = null;
+// console.log(job);
+
+// console.log(typeof (name));
+// console.log(typeof(age));
+
+// let x = 10;
+// let y = 20;
+
+// console.log( "x + y =", x + y);
