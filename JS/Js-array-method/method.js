916d9328fca0c4f6b3542a28090508bd7830f73d
Evening <evening@admins-iMac.local>
2022-12-08 19:50:21 +0800

    Dec-8 School

--- JS/Js-array-method/method.js
+++ JS/Js-array-method/method.js
@@ -3,64 +3,117 @@
 
 
 
-function brands(a) {
-
-    return a;
-
-
-
-}
-
-let carBrands = brands ([
-    ["Aston Martin Lagonda Ltd", "UK", 2016],
-    ["Audi", "Germany", 2016],
-    ["BMW", "Germany", 2016],
-    ["Chevrolet", "USA", 2016],
-    ["Dodge", "USA", 2016],
-    ["Ferrari", "Italy", 2016],
-    ["Honda", "Japan", 2016],
-    ["Jaguar", "UK", 2016],
-    ["Lamborghini", "Italy", 2016],
-]);
-
-for (let i = 0; i < carBrands.length; i++) {
-
-    let x = carBrands[i][0].split('').at(0);
-
-    if (x.toLowerCase() === "a") {
-
-        console.log('brand:' + carBrands[i][0]);
-        console.log('country:' + carBrands[i][1]);
-        console.log('year:' + carBrands[i][2]);
-
-    }
- 
-}
-
-
-console.log("==========2==========");
-
-
-for (let i = 0; i < carBrands.length; i++) {
-
-    let x = carBrands[i][1];
-
-    if ( x.toUpperCase() === "USA") {
-        console.log(carBrands[i][0] + ", " + carBrands [i][1] + ", " + carBrands[i][2]);
-        
-    }
-    
-    
-}
-
-console.log(carBrands);
-
-
 // function brands(a) {
 
-//     for (let index = 0; index < array.length; index++) {
-        
-        
-//     }
-    
+//     return a;
+
+
+
 // }
+
+// let carBrands = brands ([
+//     ["Aston Martin Lagonda Ltd", "UK", 2016],
+//     ["Audi", "Germany", 2016],
+//     ["BMW", "Germany", 2016],
+//     ["Chevrolet", "USA", 2016],
+//     ["Dodge", "USA", 2016],
+//     ["Ferrari", "Italy", 2016],
+//     ["Honda", "Japan", 2016],
+//     ["Jaguar", "UK", 2016],
+//     ["Lamborghini", "Italy", 2016],
+// ]);
+
+// for (let i = 0; i < carBrands.length; i++) {
+
+//     let x = carBrands[i][0].split('').at(0);
+
+//     if (x.toLowerCase() === "a") {
+
+//         console.log('brand:' + carBrands[i][0]);
+//         console.log('country:' + carBrands[i][1]);
+//         console.log('year:' + carBrands[i][2]);
+
+//     }
+
+// }
+
+
+// console.log("==========2==========");
+
+
+// for (let i = 0; i < carBrands.length; i++) {
+
+//     let x = carBrands[i][1];
+
+//     if ( x.toUpperCase() === "USA") {
+//         console.log(carBrands[i][0] + ", " + carBrands [i][1] + ", " + carBrands[i][2]);
+
+//     }
+
+
+// }
+
+// console.log(carBrands);
+
+
+// // function brands(a) {
+
+// //     for (let index = 0; index < array.length; index++) {
+
+
+// //     }
+
+// // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function myFunction() {
+//     alert("Hi World!");
+// }
+// myFunction();
+
+
+// let txt = "Hello World!";
+// let x = txt.length;
+// alert(x);
+
+
+
+
+// let txt = "We are \"Vikings\" ";
+// alert(txt);
+
+
+// let txt = "I can eat bananas all day";
+// let x = txt.slice(10, 17);
+
+// console.log(x);
+
+
+// let txt = "Hello World";
+// txt = txt.replace("Hello", "Welcome");
+
+// console.log(txt);
+
+
+// const cars = ["Saab", "Volvo", "BMW"];
+// let x = cars[1];
+// console.log(cars);
+// console.log(x);
+
+
+// const cars = ["Volvo", "Jeep", "Mercedes"];
+// cars[0] = "Ford";
+
+// console.log(cars);
---
+++ JS/Js-object/object.js
@@ -0,0 +1,170 @@
+// let nohoi = {
+
+// }
+
+// nohoi.uulder = "Dalmatian";
+// nohoi.name = "Kenzo";
+// nohoi.ungu = "Black and White"
+// nohoi.nas = 3;
+
+// console.log(nohoi);
+// console.log(nohoi.uulder);
+// console.log(nohoi.name);
+// console.log(nohoi.ungu);
+// console.log(nohoi.nas);
+
+
+
+// let animals = [{
+
+//     type: "dog",
+    
+//     sound: "Woof",
+
+//     printsound: function(){
+
+//         console.log("What does the" + " " + this.type + " " +"say ? -", this.sound );
+
+//     }
+
+// },
+// {
+
+//     type: "cow",
+    
+//     sound: "Moo",
+
+//     printsound: function(){
+
+//         console.log("What does the" + " " + this.type + " " +"say ? -", this.sound );
+
+//     }
+    
+
+// },
+// {
+
+//     type: "cat",
+    
+//     sound: "Meow",
+
+//     printsound: function(){
+
+//         console.log("What does the" + " " + this.type + " " +"say ? -", this.sound );
+//     }
+
+// }
+// ];
+
+// animals[0].printsound();
+// animals[1].printsound();
+// animals[2].printsound();
+
+
+// let students = [{
+     
+//     name: "Гончигсумлай", 
+//     birthOfYear: 1999, 
+//     hobby: "Car", 
+//     single: false
+// },{
+     
+//     name: "Гончиг", 
+//     birthOfYear: 2001, 
+//     hobby: "морь", 
+//     single: true
+// },{
+     
+//     name: "Сумлай", 
+//     birthOfYear: 2011, 
+//     hobby: "Computer", 
+//     single: false
+// }
+// ]
+
+// let  above20 = () => {
+
+//     let o = 2022;
+
+//     for (let i = 0; i < students.length; i++) {
+
+//         nas = o - students[i].birthOfYear;
+        
+//         if (nas > 20) {
+
+//             console.log(students[i].name);
+            
+//         }
+        
+//     }
+    
+// }
+
+// console.log(above20(students));
+
+
+
+// let age=() => {
+
+//     let o = 2022;
+
+//     for (let i = 0; i < students.length; i++) {
+
+//         nas = o - students[i].birthOfYear;
+
+//         if (nas > 20) {
+
+//             console.log(`${students[i].name} - ${nas}` );
+            
+//         }
+        
+//     }
+
+// }
+// console.log(age(students));
+
+
+
+// let status = () => {
+
+//     for (let i = 0; i < students.length; i++) {
+
+//         if (students[i].single === true ) {
+
+//             console.log(` ${students[i].name} - ${students[i].single}` );
+            
+//         }
+        
+//     }
+// }
+
+// console.log(status(students));
+
+let students=[
+    {
+    name: 'Сэд-Эрдэнэ',
+    age: 19,
+    gender: "male"
+},
+    {
+    name: 'Индра',
+    age: 19,
+    gender: "female"
+},
+    {
+    name: 'Хатнаа ',
+    age: 21,
+    gender: "male"
+},
+    {
+    name: 'Тэмүүлэн',
+    age: 23,
+    gender: "male"
+},
+    {
+    name: 'Намуун',
+    age: 23,
+    gender: "female"
+},
+]
+
